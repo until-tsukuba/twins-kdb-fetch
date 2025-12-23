a6f5368226314a7458f9d27a0e9dc8a05f044d35
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2025-12-23 13:25:52 +0900

    add linter config

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import {
+    defineConfig,
+} from "eslint/config";
+
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import js from "@eslint/js";
+
+import {
+    FlatCompat,
+} from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+    baseDirectory: new URL(".", import.meta.url).pathname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        parser: tsParser,
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        parserOptions: {},
+    },
+
+    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    "rules": {},
+}, {
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+
+        "sourceType": "script",
+        parserOptions: {},
+    },
+
+    files: ["**/.eslintrc.{js,cjs}"],
+}]);
