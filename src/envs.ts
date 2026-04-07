7b4d35da59828525a960c6be11dd0424920b6434
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2026-04-07 12:13:09 +0900

    2026に変更

--- src/envs.ts
+++ src/envs.ts
@@ -4,5 +4,5 @@ export const utid_name = process.env.UTID_NAME ?? "";
 export const password = process.env.PASSWORD ?? "";
 export const twinsHost = process.env.TWINS ?? "https://twins.tsukuba.ac.jp";
 export const kdbHost = process.env.KDB ?? "https://kdb.tsukuba.ac.jp";
-export const year = process.env.YEAR ?? "2025";
+export const year = process.env.YEAR ?? "2026";
 export const useCache = process.env.USE_CACHE?.toLowerCase() === "true";
