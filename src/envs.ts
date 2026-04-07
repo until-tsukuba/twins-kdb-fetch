700081dc4f18882169c374e1c9a7723531a69853
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2026-04-07 23:42:34 +0900

    年度ごとに変わるように

--- src/envs.ts
+++ src/envs.ts
@@ -4,5 +4,9 @@ export const utid_name = process.env.UTID_NAME ?? "";
 export const password = process.env.PASSWORD ?? "";
 export const twinsHost = process.env.TWINS ?? "https://twins.tsukuba.ac.jp";
 export const kdbHost = process.env.KDB ?? "https://kdb.tsukuba.ac.jp";
-export const year = process.env.YEAR ?? "2026";
+const academicYear = (() => {
+	const now = new Date();
+	return now.getMonth() >= 3 ? now.getFullYear() : now.getFullYear() - 1;
+})();
+export const year = process.env.YEAR ?? (academicYear + '');
 export const useCache = process.env.USE_CACHE?.toLowerCase() === "true";
